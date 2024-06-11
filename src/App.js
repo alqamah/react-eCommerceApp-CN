4c1bc8ce3315ce3501366f0ab311993220b246a5
alqamah <alqama.hasnain@gmail.com>
2024-06-11 11:33:45 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import "./App.css";
-import Items from "./components/Items";
-import Navbar from "./components/Navbar";
-import CustomItemContext from "./itemContext";
 
+// react router
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+
+
+
+// all the pages and component to render
+import Navbar from "./Component/Navbar/Navbar";
+import { Home } from "./Pages/Home";
+import {MyOrder} from "./Pages/MyOrder";
+import {Cart} from "./Pages/Cart";
+import {SignIn} from "./Pages/SignIn";
+import { SignUp } from "./Pages/SignUp";
+import { Error } from "./Pages/Error";
+
+// main app function 
 function App() {
+
+  // all the link routes
+  const router = createBrowserRouter([
+    {
+      path:"/", 
+      element: <Navbar />,
+      errorElement: <Error />,
+      children:[
+        { index:true, element: <Home />},
+        { path:"/myorder", element: <MyOrder />},
+        { path:"/cart", element: <Cart />},
+        { path:"/signin", element: <SignIn />},
+        { path:"/signup", element: <SignUp />},
+      ]
+    }
+  ]);
+
   return (
-    // providing multiple contexts
-    <CustomItemContext>
-      <div className="App">
-        <h2>Shopping Cart</h2>
-        <Navbar />
-        <Items />
-      </div>
-    </CustomItemContext>
+    <>
+      {/* routes */}
+        <RouterProvider router={router} />
+    </>
   );
 }
+
 export default App;
--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,10 +1,9 @@
-import {getFirestore} from "@firebase/firestore"
-import {getAuth} from "firebase/auth";
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,6 +17,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
 export const db = getFirestore(app);
